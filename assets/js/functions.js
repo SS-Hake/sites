919dd3a53a88586a6b88ebf0b9e89d4cd609dd20
Sam Scott-Hake <sam_hake@live.co.uk>
2015-05-16 19:17:53 +0100

    Working slider js transition. Rename and general cleanup of folder structure.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,49 +1,57 @@
-$( document ).ready(function() {
 
-  $(function() {
 
-  	//scroll to the clicked object in a time of 1 second.
-	smoothScrolling(1500);
+$(function() {
 
-	//Bring in the project info slider
-	projectSlider();
+	//scroll to the clicked object in a time of 1 second.
+smoothScrolling(1500);
+
+//Bring in the project info slider
+projectSlider();
+
+workLoad();
+
+});
+
+function smoothScrolling(duration) {
+
+	//Assign a click listener to the links containing a hash.
+	$('a[href^="#"]').on('click', function(event) {
+
+		//The clicked link is the traget.
+		var target = $( $(this).attr('href'));
+
+		//If the target exists, don't reload the page, just scroll down by the offset amount.
+		if(target.length) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			//and do it over the provided time in ms.
+			}, duration);
+		}
+	});
+}
+
+function projectSlider() {
+
+	$('.thumb-unit').click(function() {
+
+		$('.project-slider').css('left', '-100%');
+		$('.detail-container').show();
 
 	});
 
-	function smoothScrolling(duration) {
+	$('.project-return').click(function() {
 
-		//Assign a click listener to the links containing a hash.
-		$('a[href^="#"]').on('click', function(event) {
+		$('.project-slider').css('left', '0%');
+		$('.detail-container').hide(800);
+	});
 
-			//The clicked link is the traget.
-			var target = $( $(this).attr('href'));
+}
 
-			//If the target exists, don't reload the page, just scroll down by the offset amount.
-			if(target.length) {
-				event.preventDefault();
-				$('html, body').animate({
-					scrollTop: target.offset().top
-				//and do it over the provided time in ms.
-				}, duration);
-			}
-		});
-	}
+function workLoad() {
 
-	function projectSlider() {
 
-		$('.thumb-unit').click(function() {
 
-			$('.project-section').css('left', '-100%');
+}
 
-		});
-
-		$('.project-return').click(function() {
-
-			$('.project-section').css('left', '0%');
-
-		});
-
-	}
-
-});
 
